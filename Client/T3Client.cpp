62c6d6ef00203de8ca57be30c74b8c77d7dd97fd
Adam Hamot <Rjhoops92@gmail.com>
2012-02-05 23:26:28 -0500

    Client program is able to connect to server. * Takes user input and checks to see if valid IP.

--- Client/T3Client.cpp
+++ Client/T3Client.cpp
@@ -5,26 +5,44 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <cstdlib> // for system
 
 using namespace std;
 
 int main(void) {
-	cout << "Welcome to TicTacToe Online Multiplayer!" << endl;
-	cout << "Please enter a server ip (IPv4):" << endl;
 	struct addrinfo hints, *res;
+	struct sockaddr_in sa;
+	struct hostent *he;
+	char* dest[32];
 	int sockfd;
 	int con = 0;
 	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
+	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
+
+	system("clear");
+	cout << "Welcome to TicTacToe Online Multiplayer!" << endl;
+	cout << "Please enter a server ip (IPv4):" << endl;
 	string ip;
 	cin >> ip;
+	he = gethostbyname(ip.c_str());
+	while ((he = gethostbyname(ip.c_str())) == 0) {
+		cout << "Not a valid IPv4 Address!" << endl;
+		cout << "Please enter a server ip (IPv4): " << endl;
+		cin >> ip;
+	}
 	getaddrinfo(ip.c_str(), "6000", &hints, &res);
 	sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-	cout << ip << endl;
+
 	while ((con = connect(sockfd, res->ai_addr, res->ai_addrlen)) != 0)  {
 		cout << "Error with server IP " << ip << ". Please try again: " << endl;	
 		cin >> ip;
+		getaddrinfo(ip.c_str(), "6000", &hints, &res);
+		sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 	}
+
+	cout << "Successfully connected to server!" << endl;
+
+	//recv(
 	return 0;
 }
